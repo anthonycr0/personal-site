2ba6fbd413ffd91dfbcf1720e36610be556bac5d
Anthony Roberts <anthonycr0@users.noreply.github.com>
2018-08-09 22:45:51 +1000

    Added breakpoints, basic header

---
+++ src/Shared/Header/Header.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import './Header.css';
+
+
+class Header extends React.Component {
+  public render() {
+    return (
+      <header>
+        <h1>About Me</h1>
+        <hr/>
+      </header>
+    );
+  }
+}
+
+export default Header;
