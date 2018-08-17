c7c5fab3818e9e589e55ec88885952b7e3607b84
Anthony Roberts <anthonycr0@users.noreply.github.com>
2018-08-17 22:37:08 +1000

    Projects now use Grid, added project data, improvements

--- src/Sections/Projects/Project/Project.tsx
+++ src/Sections/Projects/Project/Project.tsx
@@ -2,22 +2,29 @@ import * as React from 'react';
 import './Project.css';
 
 import testProject from '../../../Images/test2.png';
+import { ProjectData } from '../Projects';
 
-class Project extends React.Component {
-  public render() {
-    return (
-      <a className="project" href="">
-        <div className="project-image">
-          <img src={testProject} alt=""/>
-        </div>
-        
-        <div className="project-info">
-          <h2 className="project-title">Streamee</h2>
-          <p className="project-desc">Australia's first online, live TV platform</p>
-        </div>
-      </a>
-    );
-  }
+export interface Props {
+  projectData: ProjectData,
+}
+
+function Project({projectData}: Props) {
+  const style = {
+    backgroundColor: projectData.color
+  };
+
+  return (
+    <a className="project" href="">
+      <div className="project-image">
+        <img src={testProject} alt=""/>
+      </div>
+      
+      <div className="project-info" style={style}>
+        <h2 className="project-title">{projectData.title}</h2>
+        <p className="project-desc">{projectData.desc}</p>
+      </div>
+    </a>
+  );
 }
 
 export default Project;
--- src/Sections/Projects/Projects.tsx
+++ src/Sections/Projects/Projects.tsx
@@ -3,19 +3,54 @@ import Header from '../../Shared/Header/Header';
 import Project from './Project/Project';
 import './Projects.css';
 
+export interface ProjectsData {
+  data: ProjectData[]
+}
+
+export interface ProjectData {
+  title: string,
+  desc: string,
+  color: string,
+  image: string
+}
+
 class Projects extends React.Component {
+
+  private projects: ProjectsData;
+
+  constructor(props: any) {
+    super(props);
+    this.projects = {
+      data: [
+        {
+          title: 'Streamee',
+          desc: `Australia's first online, live TV platform`,
+          color: '#F65800',
+          image: ''
+        },
+        {
+          title: 'Quick Maths',
+          desc: `An app based on the viral hit, Man’s Not Hot.`,
+          color: '#2C2C2C',
+          image: ''
+        }
+      ]
+    }
+  }
+
   public render() {
     return (
       <div className="projects">
         <Header title="Projects" showIcons={false}/>
         <div className="project-container">
-          <Project/>
-          <Project/>
-          <Project/>
-          <Project/>
+          {this.renderProjects()}
         </div>
       </div>
     );
+  }
+
+  private renderProjects() {
+    return this.projects.data.map((project, index) => <Project projectData={project} key={index}/>);
   }
 }
 
