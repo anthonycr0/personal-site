9b7b5b1e42a293e0665baabb5ed1a0d395464762
Anthony Roberts <anthonycr0@users.noreply.github.com>
2018-09-09 16:49:51 +1000

    Added scroll to top component for routing, improved iPhone X landscape,

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 
+import ScrollToTop from './Components/ScrollToTop';
 import Home from './Home/Home';
 import Pablo from './Pages/Pablo/Pablo';
 import PabloProjects from './Pages/Pablo/PabloProjects/PabloProjects';
@@ -12,13 +13,15 @@ class App extends React.Component {
   public render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Home}/>
-          <Route exact path="/quick-maths" component={QuickMaths}/>
-          <Route exact path="/pablo" component={Pablo}/>
-          <Route exact path="/pablo/projects" component={PabloProjects}/>
-          <Route exact path="/original" component={Original}/>
-        </Switch>
+        <ScrollToTop>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/quick-maths" component={QuickMaths}/>
+            <Route exact path="/pablo" component={Pablo}/>
+            <Route exact path="/pablo/projects" component={PabloProjects}/>
+            <Route exact path="/original" component={Original}/>
+          </Switch>
+        </ScrollToTop>
       </BrowserRouter>
     );
   }
