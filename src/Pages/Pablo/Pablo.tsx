a92fa89330dda10638fd4442213cdc78a52be32b
Anthony Roberts <anthonycr0@users.noreply.github.com>
2020-09-02 21:39:41 +1000

    Updated content, dependencies

--- src/Pages/Pablo/Pablo.tsx
+++ src/Pages/Pablo/Pablo.tsx
@@ -1,10 +1,10 @@
-import * as React from 'react';
-import { Link } from 'react-router-dom';
-import './Pablo.scss';
+import * as React from "react";
+import { Link } from "react-router-dom";
+import "./Pablo.scss";
 
-import siteContent from '../../site-content.json';
-import selfieImg from '../../Images/pablo/me.jpg';
-import inNOutImg from '../../Images/pablo/in-n-out.jpg';
+import siteContent from "../../site-content.json";
+import selfieImg from "../../Images/pablo/me.jpg";
+import inNOutImg from "../../Images/pablo/in-n-out.jpg";
 
 class Pablo extends React.Component {
   public render() {
@@ -12,47 +12,49 @@ class Pablo extends React.Component {
       <div className="pablo">
         <div className="background">
           <div className="album-cover" id="album-cover">
-            <img className="pic1" src={selfieImg} alt="A photo of Anthony"/>
-            <img className="pic2" src={inNOutImg} alt="A photo In-N-Out burger"/>
+            <img className="pic1" src={selfieImg} alt="A photo of Anthony" />
+            <img className="pic2" src={inNOutImg} alt="A photo In-N-Out burger" />
             <table className="content-table">
               <tbody>
                 <tr className="large-text">
-                  <td/>
+                  <td />
                   <td>Anthony</td>
-                  <td className="col-space"/>
+                  <td className="col-space" />
                   <td>Roberts</td>
                 </tr>
                 <tr className="large-text">
                   <td>&nbsp;</td>
                 </tr>
                 <tr className="large-text">
-                  <td/>
+                  <td />
                   <td colSpan={3}>Software Engineer</td>
                 </tr>
                 <tr className="large-text">
-                  <td/>
+                  <td />
                   <td colSpan={3}>Software Engineer</td>
                 </tr>
                 <tr className="large-text">
-                  <td/>
+                  <td />
                   <td colSpan={3}>Software Engineer</td>
                 </tr>
                 <tr className="large-text">
-                  <td/>
+                  <td />
                   <td colSpan={3}>Software Engineer</td>
                 </tr>
                 <tr className="large-text">
                   <td>&nbsp;</td>
                 </tr>
                 <tr className="large-text">
-                  <td/>
+                  <td />
                   <td>About Me</td>
                   <td colSpan={2}>Links</td>
                 </tr>
                 <tr className="small-text">
                   <td colSpan={2}>{siteContent.age}</td>
-                  <td/>
-                  <td><Link to="/pablo/projects">Projects</Link></td>
+                  <td />
+                  <td>
+                    <Link to="/pablo/projects">Projects</Link>
+                  </td>
                 </tr>
                 <tr className="small-text">
                   <td colSpan={2}>Foodie</td>
@@ -76,19 +78,24 @@ class Pablo extends React.Component {
                   <td colSpan={2}>Melbourne, AUS</td>
                 </tr>
                 <tr className="small-text">
-                  <td/>
-                  <td/>
-                  <td/>
-                  <td><a href={siteContent.links.twitter} target="_blank">Twitter</a></td>
+                  <td />
+                  <td />
+                  <td />
+                  <td />
                 </tr>
                 <tr className="small-text">
-                  <td colSpan={2}><Link to="/">← Home</Link></td>
-                  <td/>
-                  <td><a href={siteContent.links.linkedin} target="_blank">LinkedIn</a></td>
+                  <td colSpan={2}>
+                    <Link to="/">← Home</Link>
+                  </td>
+                  <td />
+                  <td>
+                    <a href={siteContent.links.linkedin} target="_blank">
+                      LinkedIn
+                    </a>
+                  </td>
                 </tr>
               </tbody>
             </table>
-
           </div>
         </div>
       </div>
@@ -101,22 +108,21 @@ class Pablo extends React.Component {
 
   public componentDidMount() {
     this.updateDimensions();
-    window.addEventListener('resize', this.updateDimensions);
+    window.addEventListener("resize", this.updateDimensions);
   }
 
   public componentWillUnmount() {
-    window.removeEventListener('resize', this.updateDimensions);
+    window.removeEventListener("resize", this.updateDimensions);
   }
 
   private updateDimensions() {
     if (document.documentElement.clientWidth >= 769) {
-      const cover = document.getElementById('album-cover');
+      const cover = document.getElementById("album-cover");
       if (cover) {
-        const style = window.getComputedStyle(cover)
-        const height = style.getPropertyValue('height');
+        const style = window.getComputedStyle(cover);
+        const height = style.getPropertyValue("height");
         cover.style.width = height;
       }
-      
     }
   }
 }
